885f2b02cedc1e8aaef4cfcf3deec02b09448dc8
Christian Smith <csmit232@kent.edu>
2024-10-16 10:39:46 -0400

    merge log sources

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,9 +1,47 @@
 "use strict";
 
+const LogSourceHeap = require("../lib/log-source-heap")
+
 // Print all entries, across all of the *async* sources, in chronological order.
 
 module.exports = (logSources, printer) => {
   return new Promise((resolve, reject) => {
-    resolve(console.log("Async sort complete."));
-  });
+    const logSourceHeap = new LogSourceHeap();
+    const promises = [];
+
+    // Initialize the heap with the earliest log entry from each source
+    logSources.map(logSource => {
+      const popPromise = logSource.popAsync()
+        .then((logEntry) => {
+          if (logEntry) {
+            logSourceHeap.insert({ logEntry, logSource });
+          }
+        })
+        .catch(reject);
+
+      promises.push(popPromise);
+    });
+
+    Promise.all(promises).then(() => resolve(logSourceHeap)).catch(reject);
+  }).then(async (logSourceHeap) => {
+    const promises = [];
+
+    while (!logSourceHeap.isEmpty()) {
+      // Get earliest log entry
+      const { logEntry, logSource } = logSourceHeap.extractMin();
+
+      printer.print(logEntry);
+
+      // Get the next log entry from the same log source
+      const nextLogPromise = logSource.popAsync().then((nextLog) => {
+        if (nextLog) {
+          logSourceHeap.insert({ logEntry: nextLog, logSource });
+        }
+      });
+
+      promises.push(nextLogPromise);
+    }
+
+    await Promise.all(promises);
+  }).then(() => printer.done());
 };
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,7 +1,34 @@
 "use strict";
 
+const LogSourceHeap = require("../lib/log-source-heap")
+
 // Print all entries, across all of the sources, in chronological order.
 
 module.exports = (logSources, printer) => {
-  return console.log("Sync sort complete.");
+  const logSourceHeap = new LogSourceHeap();
+
+  // Initialize the heap with the earliest log entry from each source
+  logSources.forEach(logSource => {
+    const logEntry = logSource.pop();
+
+    if (logEntry) {
+      logSourceHeap.insert({ logEntry, logSource });
+    }
+  });
+
+  while (!logSourceHeap.isEmpty()) {
+    // Get earliest log entry
+    const { logEntry, logSource } = logSourceHeap.extractMin();
+
+    printer.print(logEntry);
+
+    // Get the next log entry from the same log source
+    const nextLog = logSource.pop();
+    
+    if (nextLog) {
+      logSourceHeap.insert({ logEntry: nextLog, logSource });
+    }
+  }
+
+  printer.done();
 };
